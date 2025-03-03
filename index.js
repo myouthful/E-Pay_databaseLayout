245b8d23258f8a3df31b19d7802553ff016b28ef
Gideon22iboyi <your-email@example.com>
2025-03-03 14:00:39 +0100

    fixed

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const express = require('express');
 const Router= require ('./router.js');
 
-// cors would be strongly configured as more features are roled out
-// Allow requests from your frontend domain
-app.use(cors({
-    origin: 'https://e-pay-4zu3.onrender.com',  // Change this to allow only your frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }));
+
 
 
 const PORT= 3000;
 
 const app= express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'https://e-pay-4zu3.onrender.com',  // Change this to allow only your frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 
-app.use(cors())
+  app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', Router);
 
